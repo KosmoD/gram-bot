0ecc437063ae1330c72b6a6e4e6ffc1d5b05859b
SmartyPie <95571485+KosmoD@users.noreply.github.com>
2022-01-06 18:50:53 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -42,6 +42,6 @@ module.exports = {
             }],
             status: "online",
         });
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        console.log(`${client.user.tag} is online!`);
     },
 };
